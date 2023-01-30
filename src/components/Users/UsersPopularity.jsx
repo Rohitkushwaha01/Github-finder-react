3f6d44ced24d4ba77cee40027473435e0e250395
unknown <rohitkushwahac@gmail.com>
2023-01-30 16:33:56 +0530

    project completed

--- src/components/Users/UsersPopularity.jsx
+++ src/components/Users/UsersPopularity.jsx
@@ -6,21 +6,21 @@ export default function UsersPopularity() {
     const { followers, following, public_repos, login, } = user
     return (
         <div className='flex shadow-lg xl:px-10 lg:px-10 p-4 justify-between xl:w-[80%] md:w-[100%] m-auto sm:flex-wrap md:flex-nowrap flex-wrap mt-5'>
-            <div className="text-center xl:w-48 lg:w-48 md:w-40 sm:w-32 w-28 xl:border-r-2 lg:border-r-2 ">
-                <h3 className="text-amber-300 text-xl flex-grow">Followers</h3>
-                <p className='text-center text-lg text-white mt-3'>{followers}</p>
+            <div className="sm:text-center xl:w-48 lg:w-48 md:w-40 sm:w-32 w-36 border-b-2 mb-5">
+                <h3 className="text-amber-300 text-xl">Followers</h3>
+                <p className='sm:text-center text-lg text-white mt-3 '>{followers}</p>
             </div>
-            <div className="text-center xl:w-48 lg:w-48 flex-grow md:w-40 sm:w-30 w-28 xl:border-r-2 lg:border-r-2  ">
+            <div className="sm:text-center xl:w-48 lg:w-48 md:w-40 sm:w-30 w-36 mb-5 border-b-2">
                 <h3 className="text-amber-300 text-xl">Following</h3>
-                <p className='text-center text-lg text-white mt-3'>{following}</p>
+                <p className='sm:text-center text-lg text-white mt-3'>{following}</p>
             </div>
-            <div className="text-center xl:w-48 lg:w-48 md:w-40 flex-grow sm:w-30 w-28 xl:border-r-2 lg:border-r-2  ">
+            <div className="sm:text-center xl:w-48 lg:w-48 mdmb-5:w-40 sm:w-30 w-36 mb-5 border-b-2 ">
                 <h3 className="text-amber-300 text-xl">Public Repo</h3>
-                <p className='text-center text-lg text-white mt-3'>{public_repos}</p>
+                <p className='sm:text-center text-lg text-white mt-3'>{public_repos}</p>
             </div>
-            <div className="text-center xl:w-48 lg:w-48 flex-grow md:w-40 sm:w-30 w-28">
+            <div className="sm:text-center xl:w-48 lg:w-48 md:w-40 sm:w-30 w-36 mb-5 border-b-2">
                 <h3 className="text-amber-300 text-xl">UserId</h3>
-                <p className='text-center text-lg text-white mt-3'>{login}</p>
+                <p className='sm:text-center text-lg text-white mt-3'>{login}</p>
             </div>
         </div>
     )
